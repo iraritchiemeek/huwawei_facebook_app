faf4ec23b9699f43f3d5c263d0e514d5feb0d79e
iraritchiemeek <iraritchiemeek@gmail.com>
2016-03-14 08:08:51 +1300

    add thankyou message with fadeout

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-
+	$('#content').children().not('#footer_image').hide()
+	$('#content').append('<div id="thankyou_message">Thank You</div>')
+	$('#thankyou_message').delay(5000).fadeOut(400)
+	$('#content').children().not('#footer_image, #thankyou_message').delay(5000).fadeIn(400)
 })
