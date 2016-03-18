52b804eb0b8374d6e8aa0d2baaf1f852a540c143
iraritchiemeek <iraritchiemeek@gmail.com>
2016-03-18 21:35:25 +1300

    finish

--- js/controller.js
+++ js/controller.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
 	$('#content').append('<div id="thankyou_message">Thank You</div>')
 	$('#thankyou_message').delay(5000).fadeOut(400)
 	$('#content').children().not('#footer_image, #thankyou_message').delay(5000).fadeIn(400)
+
+	if (jQuery.browser.mobile == false) {
+		window.location.href = "https://www.facebook.com/HuaweiNewZealand/app/476047795919951/"
+	}
 })
